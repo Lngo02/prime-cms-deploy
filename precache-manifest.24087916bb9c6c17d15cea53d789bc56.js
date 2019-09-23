d7a54e85a2eaa5a4841a25c4542fec6afc267fd4
Linda Ngo <linda.ngo@cgic.cooperators.ca>
2019-09-23 10:35:53 -0400

    Updates

---
+++ precache-manifest.24087916bb9c6c17d15cea53d789bc56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79b0cbfef565328b9e168f8c0cd45c4",
+    "url": "/prime-cms-deploy/index.html"
+  },
+  {
+    "revision": "0d06af537e59cc27dc6f",
+    "url": "/prime-cms-deploy/static/css/main.e515da20.chunk.css"
+  },
+  {
+    "revision": "64abc5f2a77f4eed722d",
+    "url": "/prime-cms-deploy/static/js/2.0bd1f58f.chunk.js"
+  },
+  {
+    "revision": "0d06af537e59cc27dc6f",
+    "url": "/prime-cms-deploy/static/js/main.47e941c9.chunk.js"
+  },
+  {
+    "revision": "879de3587acd7a204a7e",
+    "url": "/prime-cms-deploy/static/js/runtime~main.fb9cc084.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prime-cms-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
